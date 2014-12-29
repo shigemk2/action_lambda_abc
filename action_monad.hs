0d8db5984d2c9dc9e69d5981c1d5d0c98bf3dcbd
shigemk2 <i.am.shige@gmail.com>
2014-12-29 23:45:14 +0900

    replicateM' 再実装

---
+++ action_monad.hs
@@ -0,0 +1,31 @@
+import Control.Applicative
+import System.Random
+
+replicateM' 0 _ = return []
+replicateM' n a | n > 0 = (:) <$> a <*> replicateM' (n - 1) a
+
+main = do
+    let dice = getStdRandom $ randomR (1, 6) :: IO Int
+    print =<< replicateM' 5 dice
+    putStrLn "---"
+    -- replicateM_' 3 $ do
+    --     print =<< dice
+    -- putStrLn "---"
+    -- a <- forM' [1..3] $ \i -> do
+    --     print i
+    --     return i
+    -- print a
+    -- putStrLn "---"
+    -- forM_' [1..3] $ \i -> do
+    --     print i
+    -- putStrLn "---"
+    -- let y x = x (y x)
+    -- y $ \f -> do
+    --     r <- dice
+    --     print r
+    --     when' (r /= 1) f
+    -- putStrLn "---"
+    -- y $ \f -> do
+    --     r <- dice
+    --     print r
+    --     unless' (r == 6) f
