54af3a75493231acfd69ca67a7b3647f287a679e
shigemk2 <i.am.shige@gmail.com>
2014-12-30 13:15:55 +0900

    forM' 再実装 解答

--- action_monad.hs
+++ action_monad.hs
@@ -7,6 +7,13 @@ replicateM' n a | n > 0 = (:) <$> a <*> replicateM' (n - 1) a
 replicateM_' 0 _ = return []
 replicateM_' n a | n > 0 = a >> replicateM' (n - 1) a
 
+-- forM' [] = return []
+-- forM' (x:xs) =  x >> forM' xs
+
+-- アプリカティブスタイル
+forM' [] _ = return []
+forM' (x:xs) f = (:) <$> f x <*> forM' xs f
+
 main = do
     let dice = getStdRandom $ randomR (1, 6) :: IO Int
     print =<< replicateM' 5 dice
@@ -16,11 +23,11 @@ main = do
     putStrLn "---"
     replicateM_' 3 $ print 4
     putStrLn "---"
-    -- a <- forM' [1..3] $ \i -> do
-    --     print i
-    --     return i
-    -- print a
-    -- putStrLn "---"
+    a <- forM' [1..3] $ \i -> do
+        print i
+        return i
+    print a
+    putStrLn "---"
     -- forM_' [1..3] $ \i -> do
     --     print i
     -- putStrLn "---"
