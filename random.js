4c88428c86ee7c6f648bda4fb4d3326a6c0c7d97
shigemk2 <i.am.shige@gmail.com>
2014-12-31 17:44:51 +0900

    四捨五入

--- random.js
+++ random.js
@@ -5,7 +5,7 @@ var count = [0,0,0,0,0,0,
 for(var i = 0; i < 100; i++) {
   math = 0;
   for(var j = 0; j < 12; j++) {
-    math += Math.floor(Math.random(0, 1) * 10) / 10;
+    math += Math.floor(Math.random(0, 1) * 10 + 0.5) / 10;
   }
   sum = Math.floor(math - 6);
   count[Math.floor(sum + 6)] += 1;
