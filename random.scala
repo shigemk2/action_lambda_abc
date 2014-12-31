96b21feb729c5c0ed49f95eb59a06950aa9c2cf2
shigemk2 <i.am.shige@gmail.com>
2014-12-31 22:34:00 +0900

    正規乱数 Scala

---
+++ random.scala
@@ -0,0 +1,22 @@
+import scala.util.Random
+
+var math = 0.0
+var sum = 0
+var r = new Random
+var count = new Array[Int](13)
+
+for (i <- 1 to 100) {
+  var math = 0.0
+  for (i <- 1 to 12) {
+    math += r.nextDouble
+  }
+  sum = (math + 0.5).floor.toInt
+  count(sum) += 1
+}
+for (i <- 0 to 12) {
+  val c    = i - 6
+  val star = "*" * count(i)
+  println(s"$c: $star")
+}
+
+
