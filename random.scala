5bd86602a3766422ec2a979e25cbcb973a5db1e7
7shi <7shi@live.jp>
2015-01-01 17:55:39 +0900

    _でラムダ式を簡略化

--- random.scala
+++ random.scala
@@ -4,6 +4,6 @@ val r = new Random
 val rands = (1 to 100).map(_ => (1 to 12).map(_ => r.nextDouble).sum.round.toInt - 6)
 
 for (i <- -6 to 6) {
-  val c = rands.filter(n => i == n).length
+  val c = rands.filter(i == _).length
   printf("%2d: %s\n", i, "*"*c)
 }
