f89ac83913b8defa6cd64ace258f4cfea59cb705
7shi <7shi@live.jp>
2015-01-01 17:56:03 +0900

    存在する範囲だけを表示

--- random.scala
+++ random.scala
@@ -3,7 +3,7 @@ import scala.util.Random
 val r = new Random
 val rands = (1 to 100).map(_ => (1 to 12).map(_ => r.nextDouble).sum.round.toInt - 6)
 
-for (i <- -6 to 6) {
+for (i <- rands.min to rands.max) {
   val c = rands.filter(i == _).length
   printf("%2d: %s\n", i, "*"*c)
 }
