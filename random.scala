e21cd172baaa4784f13b88846630dab51a26a3b9
shigemk2 <i.am.shige@gmail.com>
2015-01-01 16:55:03 +0900

    正規乱数 Scala 再実装

--- random.scala
+++ random.scala
@@ -1,23 +1,11 @@
 import scala.util.Random
 
-var math = 0.0
-var sum = 0
-var r = new Random
-var count = new Array[Int](13)
+val r = new Random
+val rands = (1 to 100).flatMap(_ => List((1 to 12).flatMap(_ => List(r.nextDouble)).sum.+(0.5).round.toInt - 6))
 
-for (i <- 1 to 100) {
-  var math = 0.0
-  for (i <- 1 to 12) {
-    math += r.nextDouble
-  }
-  sum = (math + 0.5).floor.toInt
-  count(sum) += 1
+for (i <- -6 to 6) {
+  val c = rands.filter(n => i == n).length
+  val j = i.toString
+  val n = " " * (2 - j.length) + j
+  printf("%s: %s\n", n, "*"*c)
 }
-for (i <- 0 to 12) {
-  val c    = i - 6
-  val spc  = " " * (2 - c.toString.length)
-  val star = "*" * count(i)
-  println(s"$spc$c: $star")
-}
-
-
