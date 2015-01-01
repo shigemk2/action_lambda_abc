7159d5d6094f0aaa8d2e701ac14f778a062255aa
7shi <7shi@live.jp>
2015-01-01 17:53:09 +0900

    "%2d"で桁揃え

--- random.scala
+++ random.scala
@@ -5,7 +5,5 @@ val rands = (1 to 100).map(_ => (1 to 12).map(_ => r.nextDouble).sum.round.toInt
 
 for (i <- -6 to 6) {
   val c = rands.filter(n => i == n).length
-  val j = i.toString
-  val n = " " * (2 - j.length) + j
-  printf("%s: %s\n", n, "*"*c)
+  printf("%2d: %s\n", i, "*"*c)
 }
