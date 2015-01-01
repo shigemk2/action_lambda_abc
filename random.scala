cc011b01d571a96917e3b7f3c64cd7dca4e6e564
7shi <7shi@live.jp>
2015-01-01 17:52:22 +0900

    flatMap -> map, +(,5).floor -> round

--- random.scala
+++ random.scala
@@ -1,7 +1,7 @@
 import scala.util.Random
 
 val r = new Random
-val rands = (1 to 100).flatMap(_ => List((1 to 12).flatMap(_ => List(r.nextDouble)).sum.+(0.5).floor.toInt - 6))
+val rands = (1 to 100).map(_ => (1 to 12).map(_ => r.nextDouble).sum.round.toInt - 6)
 
 for (i <- -6 to 6) {
   val c = rands.filter(n => i == n).length
