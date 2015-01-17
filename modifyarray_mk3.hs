e65d82d7f4fe9baed110d227a66729402d14a2b4
shigemk2 <i.am.shige@gmail.com>
2015-01-17 19:15:53 +0900

    modifyArrayをアプリカティブスタイルを使って実装する軌跡

--- modifyarray_mk3.hs
+++ modifyarray_mk3.hs
@@ -1,9 +1,15 @@
 import Data.IORef
 import Data.Array.IO
+import Control.Applicative
 
-modifyArray a x f = do
-    a' <- readArray a x
-    writeArray a x (f a')
+-- 無理してアプリカティブスタイルで書く必要はないけどアプリカティブスタイルで書くこともできる
+modifyArray a x f =
+    -- readArray a x >>= \a' -> writeArray a x (f a')
+    -- readArray a x >>= \a' -> (return $ f a') >>= writeArray a x
+    -- readArray a x >>= \a' -> (return .f) a' >>= writeArray a x
+    -- readArray a x >>= return.f >>= writeArray a x
+    -- writeArray a x =<< return.f =<< readArray a x
+    writeArray a x =<< f <$> readArray a x
 
 main = do
     a <- newArray (0, 2) 0 :: IO (IOUArray Int Int)
