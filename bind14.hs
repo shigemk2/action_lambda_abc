2d98483a7d72b9d38ae3f461ae8f391a73c54ebe
shigemk2 <i.am.shige@gmail.com>
2015-01-17 19:12:00 +0900

    アプリカティブスタイルを使おう

--- bind14.hs
+++ bind14.hs
@@ -1,7 +1,14 @@
+import Control.Applicative
+
 inc x = x + 1
 
 main = do
     (return . inc) 2 >>= print
     print $ inc 2
-    -- return 2 >>= inc >>= print
+    return 2 >>= return.inc >>= print
+    print =<< return.inc =<< return 2
     -- print =<< inc =<< return 2
+    -- アプリカティブスタイル
+    -- 関数合成がわからないと有り難みがわからないかもしれない
+    -- incの戻り値をアクションにしようとしている
+    print =<< inc <$> return 2
