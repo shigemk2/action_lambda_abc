7d62119e0de0cccf3d94351cee5696130909da77
shigemk2 <i.am.shige@gmail.com>
2015-01-17 18:44:13 +0900

    IORefの補足

--- ioref.hs
+++ ioref.hs
@@ -1,8 +1,10 @@
+-- newしてreadしてwriteする破壊的代入
 import System.Random
 import Data.IORef
 
 main = do
-    a <- newIORef =<< (getStdRandom $ randomR (0, 9) :: IO Int)
+    -- a <- newIORef =<< (getStdRandom $ randomR (0, 9) :: IO Int)
+    a <- newIORef =<< return 1
     -- a <- newIORef 2
     a' <- readIORef a
     writeIORef a (a' + 1)
