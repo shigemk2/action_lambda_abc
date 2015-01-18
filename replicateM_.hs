1413a1255fd2b80ec45cb0ff9042949f4a48bfec
shigemk2 <i.am.shige@gmail.com>
2015-01-18 14:11:59 +0900

    replicateM_を1行で

--- replicateM_.hs
+++ replicateM_.hs
@@ -5,6 +5,7 @@ dice :: IO Int
 dice = getStdRandom $ randomR (1, 6)
 
 main = do
-    replicateM_ 3 $ do
-        print =<< dice
-    replicateM_ 3 $ print 4
+    -- replicateM_ 3 $ do
+    --     print =<< dice
+    replicateM_ 3 $ print =<< dice
+    -- replicateM_ 3 $ print 4
